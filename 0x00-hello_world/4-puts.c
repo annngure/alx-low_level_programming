890a450dd582e162c8d72219207b3e2ce0b5c546
annngure <annwanjikungure@gmail.com>
2023-03-06 12:19:30 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
-*main - Put function is used to print a new line in c
-*Return: 0 (Success)
-*/
+ *main - Put function is used to print a new line in c
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+        puts("\"Programming is like building a multilingual puzzle
+");
+	return (0);
 }
